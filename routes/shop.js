48f7764aa07bf9d38ad0d1161724becc99e6948c
adelazamfirescu <adela.zamfirescu@vodafone.com>
2021-11-12 15:00:43 +0200

    added color variables and shop route

---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const axios = require("axios").default;
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("shop", { title: "Shop" });
+});
+
+module.exports = router;
