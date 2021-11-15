32b5315c866ac3fa516a2c837d4bb539d52226af
adelazamfirescu <adela.zamfirescu@vodafone.com>
2021-11-15 15:57:17 +0200

    added ejs template for shop

--- routes/shop.js
+++ routes/shop.js
@@ -8,10 +8,34 @@ var router = express.Router();
 let rawdata = fs.readFileSync(path.resolve(__dirname, "../productList.json"));
 let result = JSON.parse(rawdata);
 let productList = result.products;
+const brands = [...new Set(productList.map((product) => product.brand))];
+const os = [
+  ...new Set(
+    productList.map((product) =>
+      product.operating_system ? product.operating_system : "N/A"
+    )
+  ),
+];
+const propertiesSort = [
+  { id: "d", text: "Choose option" },
+  { id: "a_r", text: "Sort ascending by rating" },
+  { id: "a_p", text: "Sort ascending by price" },
+  { id: "d_r", text: "Sort descending by rating" },
+  { id: "d_p", text: "Sort descending by price" },
+];
+
+const availability = ["all", "until_today"];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("shop", { title: "Shop", productList: productList });
+  res.render("shop", {
+    title: "Shop",
+    productList: productList,
+    brands: brands,
+    os: os,
+    propertiesSort: propertiesSort,
+    propertiesAvailability: availability,
+  });
 });
 
 /* GET product list. */
