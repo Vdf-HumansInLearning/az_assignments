a39aeed8f624ca5130792e531e0bf850d493a721
adelazamfirescu <adela.zamfirescu@vodafone.com>
2021-11-22 13:12:40 +0200

    added auth route in app

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var shopRouter = require("./routes/shop");
 // var detailsRouter = require("./routes/details");
-var loginRouter = require("./routes/login");
-var registerRouter = require("./routes/register");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
@@ -27,8 +26,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/shop", shopRouter);
 app.use("/shop/product", shopRouter);
-app.use("/login", loginRouter);
-app.use("/register", registerRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
