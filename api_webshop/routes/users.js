0f479d218ccc46b483b79e87bbfeda89f38b59e4
adelazamfirescu <adela.zamfirescu@vodafone.com>
2021-11-26 19:08:37 +0200

    added put route for users

--- api_webshop/routes/users.js
+++ api_webshop/routes/users.js
@@ -18,9 +18,8 @@ router.get("/users", (req, res) => {
 
 /* GET one user */
 router.get("/users/:id", (req, res) => {
-  //verify if the id is a number
-  //if not, message: please enter a valid number
   console.log(req.params.id);
+
   let foundUser = userList.find((user) => user.id == req.params.id);
   if (foundUser) {
     res.status(200).send(foundUser);
@@ -45,7 +44,6 @@ router.get("/regulars", (req, res) => {
 
 /* POST user */
 router.post("/users", (req, res) => {
-  //verify if necessary fields are present
   if (req.body.email && req.body.password && req.body.username) {
     const user = {
       id: userList[userList.length - 1].id + 1,
@@ -55,15 +53,12 @@ router.post("/users", (req, res) => {
       isAdmin: false,
     };
 
-    //validate user
     let result = utils.validateNewUser(user);
 
-    //verify if the user already exists
     let verifyUser = userList.find((user) => user.email == req.body.email);
     if (verifyUser) {
       res.status(403).send({ message: "User already registered" });
     } else if (result.isValid) {
-      //push user to array and write to file
       userList.push(user);
       let json = JSON.stringify(users, null, 2);
       fs.writeFile(
@@ -84,25 +79,16 @@ router.post("/users", (req, res) => {
 
 /* DELETE user */
 router.delete("/users", (req, res) => {
-  //verify if necessary fields are present
   if (req.body.email) {
-    //validate if email is correct
-    //find user
     let foundUser = userList.find((user) => user.email == req.body.email);
     if (foundUser) {
-      //delete user
-      //get index of user
-
       let userIndex = userList.findIndex(
         (item) => item.email === req.body.email
       );
       console.log(userIndex);
       if (userIndex !== -1) {
-        //if we found the index, remove the user with the given email
-
         let removed = userList.splice(userIndex, 1);
         console.log(removed);
-        //update file
         let json = JSON.stringify(users, null, 2);
         fs.writeFile(
           path.resolve(__dirname, "../users.json"),
@@ -125,10 +111,31 @@ router.delete("/users", (req, res) => {
   }
 });
 
+/* PUT user */
 //modify a user admin status
-router.put("/users/", (req, res) => {
-  //verify if necessary fields are present
+router.put("/users", (req, res) => {
   if (req.body.email) {
+    let foundIndex = userList.findIndex(
+      (item) => item.email === req.body.email
+    );
+    console.log(foundIndex);
+    if (foundIndex) {
+      userList[foundIndex].isAdmin = !userList[foundIndex].isAdmin;
+      let json = JSON.stringify(users, null, 2);
+      fs.writeFile(
+        path.resolve(__dirname, "../users.json"),
+        json,
+        function (err) {
+          if (err) throw err;
+
+          res.status(200).send({
+            message: `User with email ${userList[foundIndex].email} was updated`,
+          });
+        }
+      );
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
   } else {
     res.status(400).send({ message: "Please complete all fields" });
   }
