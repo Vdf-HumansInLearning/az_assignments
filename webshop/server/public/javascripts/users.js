e3ad52c1cbd1e951da649c90170d6020530f454c
Adela Zamfirescu <adela.zamfirescu@vodafone.com>
2021-12-10 18:50:45 +0200

    delete users ejs

--- webshop/server/public/javascripts/users.js
+++ webshop/server/public/javascripts/users.js
@@ -1,26 +1,38 @@
-let baseUrl = "http://localhost:8080/api/users/";
+const baseUrl = "http://localhost:8080/api/users/";
+let userIdDelete = null;
 
-// function editUser(userId) {
-//   fetch(baseUrl + "userId", {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   })
-//     .then((response) => {
-//       response.json();
-//     })
-//     .then((data) => {
-//       location.reload();
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// }
+function editUser(user) {
+  fetch(baseUrl + user.id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      location.reload();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
-function deleteUser(userId) {
-  let id = Number(userId);
-  fetch(baseUrl + userId, {
+function deleteUser(user) {
+  userIdDelete = user.id;
+  let deleteModal = document.getElementById("confirmDelete");
+  let modalText = deleteModal.querySelector(".modal-body p");
+  let text = document.createTextNode(
+    `Email: ${user.email} Username: ${user.username}`
+  );
+  modalText.appendChild(text);
+}
+
+function confirmDelete() {
+  console.log(userIdDelete);
+  fetch(baseUrl + userIdDelete, {
     method: "DELETE",
   })
     .then((response) => {
@@ -29,6 +41,14 @@ function deleteUser(userId) {
     .then((data) => {
       console.log(data);
       location.reload();
+
+      let liveToast = document.getElementById("liveToast");
+      let toastBody = document.querySelector("#liveToast .toast-body");
+      let text = document.createTextNode(data.message);
+      toastBody.appendChild(text);
+      let toast = new bootstrap.Toast(liveToast);
+      toast.show();
+      console.log(liveToast);
     })
     .catch((error) => {
       console.error("Error:", error);
